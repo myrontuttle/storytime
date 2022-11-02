269bc4eefb1f82a14930dfb42eca4e3626e5768c
Myron Tuttle <myrongt@gmail.com>
2022-11-01 18:50:42 -0600

    Initial commit

---
+++ storytime/__init__.py
@@ -0,0 +1,18 @@
+"""Storytime is an experiment in continuous storytelling."""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return str(importlib_metadata.version(__name__))
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
